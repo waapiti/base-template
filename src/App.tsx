06dfed87ecced99635ca2a87d92f7cc4b678f59f
novoa97 <novoa97@gmail.com>
2023-08-23 12:08:57 +0200

    Base template V5

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import { TemplateContext} from '@waapiti/templates';
+
+import './App.css';
+
+function App() {
+  // const { vars } = React.useContext(TemplateContext);
+
+  return <div className='template'></div>;
+}
+
+export default App;
